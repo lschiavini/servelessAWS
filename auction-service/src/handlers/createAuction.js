ae73e95249990d5ac234607e2f707a93d11fc482
Lucas Skira <lucas.schiavini@coreconsulting.com.br>
2020-10-03 20:22:31 -0300

    setting up env

---
+++ auction-service/src/handlers/createAuction.js
@@ -0,0 +1,33 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+
+async function createAuction(event, context) {
+
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createAt: now.toISOString(),
+  };
+
+  await dynamoDB.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
+
+
